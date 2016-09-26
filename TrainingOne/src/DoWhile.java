a517cb154eff35b60d8efedae2acec75401c4ebb
Sharan Angadi <sharan@mathrusoft.com>
2016-09-26 12:45:20 +0530

    T2

---
+++ TrainingOne/src/DoWhile.java
@@ -0,0 +1,17 @@
+public class DoWhile {
+
+	public static void main(String[] args) {
+
+		int n = 0;
+		do {
+			n++;
+			System.out.println("Inside DO_WHILE" + n);
+		} while (n < 10);
+
+//		while (n > 10) {
+//			System.out.println("Inside WHILE" + n);
+//		}
+
+	}
+
+}
---
+++ TrainingOne/src/LoopFor.java
@@ -0,0 +1,22 @@
+public class LoopFor {
+
+	public static void main(String[] args) {
+
+		for (int i = 0; i < 10; i = i + 2) {
+			System.out.println(i);
+		}
+		
+//		for (int i = 0; i < 10; i = i + 2, i = 0) {
+//			System.out.println(i);
+//		}
+
+//		int i = 0;
+//		while (i < 10) {
+//			System.out.println(i);
+//			i = i + 2;
+//			i = 0;
+//		}
+
+	}
+
+}
